2abb495c2829207d9884a5544ddc071851873b24
thiagosng <thiagonunes026@gmail.com>
2021-10-28 23:26:29 -0300

    [FEAT] Pagina inicial após login

---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { userActions } from '../../redux/actions';
+
+function HomePage() {
+    const users = useSelector(state => state.users);
+    const user = useSelector(state => state.authentication.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(userActions.getAll());
+    }, []);
+
+    function handleDeleteUser(id) {
+        dispatch(userActions.delete(id));
+    }
+
+    return (
+        <div className="col-lg-8 offset-lg-2">
+            <h1>Oi {user.firstName}!</h1>
+            <p>Você está logado!!</p>
+            <h3>Todos os usuários registrados:</h3>
+            {users.loading && <em>Carregando usuários...</em>}
+            {users.error && <span className="text-danger">ERROR: {users.error}</span>}
+            {users.items &&
+                <ul>
+                    {users.items.map((user, index) =>
+                        <li key={user.id}>
+                            {user.firstName + ' ' + user.lastName}
+                            {
+                                user.deleting ? <em> - Deleting...</em>
+                                : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
+                                : <span> - <a onClick={() => handleDeleteUser(user.id)} className="text-primary">Deletar</a></span>
+                            }
+                        </li>
+                    )}
+                </ul>
+            }
+            <p>
+                <Link to="/login">Deslogar</Link>
+            </p>
+        </div>
+    );
+}
+
+export { HomePage };
